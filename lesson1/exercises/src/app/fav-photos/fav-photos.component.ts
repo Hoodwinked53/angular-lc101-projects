a65cf0c0af098f18e0ef1d53e4f825d190b4da4e
Hoodwinked53 <78771132+Hoodwinked53@users.noreply.github.com>
2021-03-29 14:55:03 -0500

    Updated the site with updated chores and movies styling, along with new images.

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Some of my favorite photos!';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://lh3.googleusercontent.com/proxy/UenhyFEkj3QK6x6ivqMjDXE7QEfBqdw6ZxqU6qgbXVeBirfWyCIvbGMEi_k2423LfvZ1t--E7Vx_-P82BADYK5b0zq311AoxWAiyXZxN24ffx1_Ag7p4sbJnSIOzZbztVB4d5C18wihcif167STiiAu2eWB7YwrhRQUhn0rB5dlqR9wWFG4L3w';
+  image3 = 'https://mostateparks.com/sites/mostateparks/files/flickrcache/72157625843213481/32776402461.jpg';
 
   constructor() { }
 

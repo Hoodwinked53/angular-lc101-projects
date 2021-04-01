46a8c0a044f03b37b64223876b1334b665bb7c4e
Hoodwinked53 <78771132+Hoodwinked53@users.noreply.github.com>
2021-04-01 13:27:16 -0500

    Finished up input example from reading.

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,25 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+
+   // addMovie (newTitle: string) {
+   //    if(!this.movies.includes(newTitle)) {
+   //       this.movies.push(newTitle);
+   //    }
+   // }
+
+   addMovie (newTitle: string) {
+      let errorMsg = '';
+      if (newTitle === '') {
+         errorMsg = 'Please enter a movie title.';
+      } else if (this.movies.includes(newTitle)) {
+         errorMsg = `${newTitle} is already in your movie list.`;
+      } else {
+         this.movies.push(newTitle);
+      }
+      return errorMsg;
+   }
+
 }
+
+// Common practice to put constructor and functions like ngOnInit AFTER the vairable declarations but BEFORE any custom functions
